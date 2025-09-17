a7158fa1cf4dadc4f78b24a93e71bbca79cae81a
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-09-17 13:16:37 +0300

    print stack trace

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -5,9 +5,10 @@ package org.example.app;
 
 import org.example.list.LinkedList;
 
+import static org.example.app.MessageUtils.getMessage;
+import static org.example.utilities.DebugUtils.printStackTrace;
 import static org.example.utilities.StringUtils.join;
 import static org.example.utilities.StringUtils.split;
-import static org.example.app.MessageUtils.getMessage;
 
 import org.apache.commons.text.WordUtils;
 
@@ -17,5 +18,7 @@ public class App {
         tokens = split(getMessage());
         String result = join(tokens);
         System.out.println(WordUtils.capitalize(result));
+
+        printStackTrace();
     }
 }
---
+++ utilities/src/main/java/org/example/utilities/DebugUtils.java
@@ -0,0 +1,15 @@
+package org.example.utilities;
+
+public class DebugUtils {
+
+    // https://stackoverflow.com/a/45944850
+    public static void printStackTrace() {
+        System.out.println("Printing stack trace:");
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        for (int i = 1; i < elements.length; i++) {
+            StackTraceElement s = elements[i];
+            System.out.println("\tat " + s.getClassName() + "." + s.getMethodName() + "(" + s.getFileName() + ":" + s.getLineNumber() + ")");
+        }
+    }
+
+}
