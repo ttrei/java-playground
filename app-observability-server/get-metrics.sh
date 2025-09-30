d08ed86b20d057032eee53938d8fcf0bf407e236
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-09-30 16:47:00 +0300

    work

--- app-observability-server/get-metrics.sh
+++ app-observability-server/get-metrics.sh
@@ -4,12 +4,25 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 cd "$SCRIPT_DIR" || exit 1
 
-curl localhost:7654/user/123 > /dev/null &
-curl localhost:7654/user/123/full > /dev/null &
-curl -X POST localhost:7654/user/123/ping > /dev/null &
-curl localhost:7654/user/555/full > /dev/null &
-curl -X POST localhost:7654/user/555/ping > /dev/null &
-curl localhost:7654/user/555 > /dev/null &
-wait
+# parallel
+if [ "$1" = "p" ]; then
+    curl localhost:7654/user/123 > /dev/null &
+    curl localhost:7654/user/123/full > /dev/null &
+    curl -X POST localhost:7654/user/123/ping > /dev/null &
+    curl localhost:7654/user/555/full > /dev/null &
+    curl -X POST localhost:7654/user/555/ping > /dev/null &
+    curl localhost:7654/user/555 > /dev/null &
+    wait
+fi
+
+# sequential
+if [ "$1" = "s" ]; then
+    curl localhost:7654/user/123 > /dev/null
+    curl localhost:7654/user/555 > /dev/null
+    curl localhost:7654/user/123/full > /dev/null
+    curl localhost:7654/user/555/full > /dev/null
+    curl -X POST localhost:7654/user/123/ping > /dev/null
+    curl -X POST localhost:7654/user/555/ping > /dev/null
+fi
 
 curl localhost:7654/actuator/prometheus
