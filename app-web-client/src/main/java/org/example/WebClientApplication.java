de7ff00823edbb19a0dbed63ac543f6b9cd26c5f
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-10-16 09:08:29 +0300

    web client

--- app-web-client/src/main/java/org/example/WebClientApplication.java
+++ app-web-client/src/main/java/org/example/WebClientApplication.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import org.example.service.ContentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebClientApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(ApplicationContext ctx, ContentService contentService) {
+        return args -> {
+            System.out.println("Executing code...");
+            contentService.run();
+            int exitCode = SpringApplication.exit(ctx, () -> 0);
+            System.exit(exitCode);
+        };
     }
 }
--- app-web-client/src/main/java/org/example/service/ContentService.java
+++ app-web-client/src/main/java/org/example/service/ContentService.java
@@ -1,13 +1,11 @@
 package org.example.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
@@ -15,23 +13,22 @@ public class ContentService {
 
     private final WebClient webClient;
 
-    public ContentService(@Value("${content-service}") String baseURL) {
-        this.webClient = WebClient.builder().baseUrl(baseURL)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ContentService() {
+        this.webClient = WebClient.builder().baseUrl("localhost")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .filter(logRequest())
                 .build();
     }
 
-    @Override
-    public Mono<Post> getPost(int id) {
-        return webClient.get().uri("posts/{id}", id).retrieve().bodyToMono(Post.class);
+    public void run() {
+        log.info("ContentService run");
     }
 
     private ExchangeFilterFunction logRequest() {
         return (clientRequest, next) -> {
-            log.info("Request: {} {}" + clientRequest.method() + clientRequest.url());
+            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
             clientRequest.headers()
-                    .forEach((name, values) -> values.forEach(value -> log.info("{}={}" + name + value)));
+                    .forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
             return next.exchange(clientRequest);
         };
     }
