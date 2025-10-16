c3ad615bc70ad3b9cdc6c20f4aa49cc36ae0ee76
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-10-16 09:09:35 +0300

    move

--- app-web-client/src/main/java/org/example/WebClientApplication.java
+++ app-web-client/src/main/java/org/example/WebClientApplication.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.service.ContentService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
